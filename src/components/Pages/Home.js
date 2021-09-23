302d38ab55451da5f261827e59716e4c151fe181
Sajan Gutta <gutta@usc.edu>
2021-09-22 23:20:38 -0700

    Implementing Home page desing.

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Button from "../Button"
+import "./Home.css"
+
 
 function Home() {
+    const buttonClick = () => {
+        console.log("Clicked!");
+    }
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="home-container">
+            <div className="home-header-container">
+
+            </div>
+            <h1 className="home-header">A New Way To Donate</h1>
+            <div className="home-details-container">
+                <p className="home-details-paragraph">Deposit Assets.</p>
+                <p className="home-details-paragraph">Accrue Interest.</p>
+                <p className="home-details-paragraph">Donate A % Of Your Interest To Charity.</p>
+            </div>
+            <div className="home-buttons-container">
+                <NavLink exact to="/create">
+                    <Button isDark={true} onClick={buttonClick}>I'm a Charity</Button>
+                </NavLink>
+                <NavLink exact to="/connect">
+                    <Button>Get Started</Button>
+                </NavLink>
+            </div>
         </div>
     )
 }
