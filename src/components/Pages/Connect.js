ebf710e3e08de912dcbbadb695b71b3006a75743
Sajan Gutta <gutta@usc.edu>
2021-10-07 13:07:40 -0700

    Updating connect page to allow connecting to a wallet provider via web3modal. This supports metamask and walletconnect.

--- src/components/Pages/Connect.js
+++ src/components/Pages/Connect.js
@@ -1,31 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Page from "../Page/Page"
 import Card from "../Card/Card"
 import Button from "../Button/Button"
-import { useDispatch } from "react-redux"
-import { testToggle } from "./../../redux/actions/test"
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 import "./Connect.css"
 
 
 const Connect = () => {
-    const dispatch = useDispatch();
+    // Storing provider state to be able to display info on page.
+    const [provider, setProvider] = useState(null);
 
-    const handleClick = () => {
-        dispatch(testToggle());
+    // Defining provider options to add wallet connect to web3modal
+    const providerOptions = {
+        walletconnect: {
+            package: WalletConnectProvider,
+            options: {
+                infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+            }
+        },
+    };
+
+    // Initializing web3modal
+    const web3Modal = new Web3Modal({
+        network: "mainnet",
+        cacheProvider: true, // optional
+        disableInjectedProvider: false,
+        providerOptions // required
+    });
+
+    // Connect to cached provider if any.
+    useEffect(async() => {
+        if (!provider && web3Modal.cachedProvider) {
+            let new_provider = await web3Modal.connect();
+            setProvider(new_provider);
+        }
+    }, []);
+
+    // Disconnecting provider
+    const disconnect = async () => {
+        // Close provider if necessary
+        if (provider && provider.close) {
+            await provider.close();
+        }
+
+        // Clear web3modal cached provider and set provider state back to null
+        await web3Modal.clearCachedProvider();
+        setProvider(null);
+    }
+
+    const connect = async () => {
+        const new_provider = await web3Modal.connect();
+        setProvider(new_provider);
+    };
+
+    // Determining display name for connected provider (if there is one)
+    let connectedProviderName = "----";
+    if (provider && web3Modal.cachedProvider) {
+        if (web3Modal.cachedProvider == "injected") {
+            connectedProviderName = "Metamask"
+        } else {
+            connectedProviderName = web3Modal.cachedProvider
+        }
     }
 
     return (
         <Page>
             <div className="connect-card-container">
-                <Card name="Connected Address" value="XXXXXXXXXXXXXXXXXXXXX"/>
+                <Card name="Connected Address" value={provider ? provider.selectedAddress : "XXXXXXXXXXXXXXXXXXXXX"} />
             </div>
             <div className="connect-card-container">
-                <Card name="Wallet Provider" value="Metamask"/>
+                <Card name="Wallet Provider" value={connectedProviderName} />
             </div>
             <div className="connect-buttons-container">
-                    <Button onClick={handleClick} isDark={true}>Disconnect</Button>
-                    <Button>Connect</Button>
-                </div>
+                <Button onClick={disconnect} isDark={true}>Disconnect</Button>
+                <Button onClick={connect}>Connect</Button>
+            </div>
         </Page>
     )
 }
