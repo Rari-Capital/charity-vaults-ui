cb2a7a38b2cac9a96ea4c4770ba76fc1149ea6cc
Sajan Gutta <gutta@usc.edu>
2021-10-10 12:38:51 -0700

    Connecting input fields to state

--- src/components/Pages/Create.js
+++ src/components/Pages/Create.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Page from "../Page/Page"
 import Button from "../Button/Button"
 import Input from "../Input/Input"
@@ -7,24 +7,34 @@ import "./Create.css"
 
 
 const Create = () => {
+    const [charityAddress, setCharityAddress] = useState(null);
+    const [giftRate, setGiftRate] = useState(null);
+    const [charityName, setCharityName] = useState(null);
+
     return (
         <Page>
             <div className="create-container">
                 <InputHeader value="CHARITY WALLET ADDRESS" />
                 <div className="large-input-container">
-                    <Input type="text" placeholder="XXXXXXXXXXXXXXXXX" />
+                    <Input value={charityAddress} 
+                    onChange={(event) => {setCharityAddress(event.target.value)}} 
+                    type="text" placeholder="XXXXXXXXXXXXXXXXX" />
                 </div>
                 <div className="double-input-container">
                     <div className="small-input-combo-container">
                         <InputHeader value="INTEREST GIFT RATE" />
                         <div className="small-input-container">
-                        <Input type="number" placeholder="XXXXXXXXXXXXXXXXX" />
+                        <Input value={giftRate} 
+                        onChange={(event) => {setGiftRate(event.target.value)}} 
+                        type="number" placeholder="XXXXXXXXXXXXXXXXX" />
                         </div>
                     </div>
                     <div className="small-input-combo-container">
                         <InputHeader value="CHARITY NAME" />
                         <div className="small-input-container">
-                        <Input type="text" placeholder="XXXXXXXXXXXXXXXXX" />
+                        <Input value={charityName} 
+                        onChange={(event) => {setCharityName(event.target.value)}} 
+                        type="text" placeholder="XXXXXXXXXXXXXXXXX" />
                         </div>
                     </div>
                 </div>
