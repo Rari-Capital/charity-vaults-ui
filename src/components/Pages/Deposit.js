e998c16d24b16a65c97f8344db64d5341b72696e
Rohun Carlton <rcarlton@usc.edu>
2021-10-16 20:50:07 -0700

    modified deposit page

--- src/components/Pages/Deposit.js
+++ src/components/Pages/Deposit.js
@@ -1,11 +1,115 @@
-import React from 'react'
+// import React from 'react'
+// import Page from "../Page/Page"
+import React, { useState } from 'react'
 import Page from "../Page/Page"
+import Button from "../Button/Button"
+import Input from "../Input/Input"
+import Modal from "../Modal/Modal"
+import InputHeader from "../Input/InputHeader"
+//import "./Create.css"
 
+// const Deposit = () => {
+//     const [charityAddress, setCharityAddress] = useState(null);
+//     const [giftRate, setGiftRate] = useState(null);
+//     const [charityName, setCharityName] = useState(null);
 
-const Deposit = () => {
+//     const [showReferralModal, setShowReferralModal] = useState(false);
+//     const [referralLink, setReferralLink] = useState(null)
+
+//     const generateReferralLink = () => {
+//         var getUrl = window.location;
+//         var baseUrl = getUrl .protocol + "//" + getUrl.host + "/";
+        
+//         let referralLink;
+//         if (!charityAddress) {
+//             referralLink = "Must provide a charity wallet address.";
+//         } else if (!giftRate) {
+//             referralLink = "Must provide a gift rate.";
+//         } else if (!charityName) {
+//             referralLink = "Must provide a charity name.";
+//         } else {
+//             referralLink = `${baseUrl}deposit?address=${charityAddress}&rate=${giftRate}&name=${charityName}`;
+//         }
+        
+//         setReferralLink(referralLink);
+//         setShowReferralModal(true);
+// 	}
+	
+	const Deposit = () => {
+		const [charityAddress, setCharityAddress] = useState(null);
+		const [customInterestRate, setCustomInterestRate] = useState(null);
+		const [currency, setCurrecncy] = useState(null);
+		const [depositAmount, setDepositAmount] = useState(null);
+
+		const [showReferralModal, setShowReferralModal] = useState(false);
+    	const [referralLink, setReferralLink] = useState(null)
+	
+		const doDeposit = () => {
+			var getUrl = window.location;
+			var baseUrl = getUrl .protocol + "//" + getUrl.host + "/";
+			
+			let referralLink;
+			if (!charityAddress) {
+				referralLink = "Must provide a charity wallet address.";
+			} else if (!customInterestRate) {
+				referralLink = "Must provide a gift rate.";
+			} else if (!currency) {
+				referralLink = "Must provide a charity name.";
+			} else if (!depositAmount) {
+				referralLink = "Must provide deposit amount.";
+			} else {
+				referralLink = `${baseUrl}deposit?address=${charityAddress}&rate=${customInterestRate}&name=${currency}&depositAmount=${depositAmount}`;
+			}
+			
+			setReferralLink(referralLink);
+			setShowReferralModal(true);
+		}
+
     return (
         <Page>
-            <h1>Deposit</h1>
+            <div className="create-container">
+                <div className="small-input-container">
+					<div>
+						<InputHeader value="SELECT CHARITY" />
+						<Input value={charityAddress} 
+						onChange={(event) => {setCharityAddress(event.target.value)}} 
+						type="text" placeholder="XXXXXXXXXXXXXXXXX" />
+					</div>
+					<div>
+						<InputHeader value="ENTER CUSTOM IINTEREST RATE" />
+						<Input value={customInterestRate}
+						onChange={(event) => {setCustomInterestRate(event.target.value)}}
+						type="text" placeholder="X.XX%"/>
+					</div>
+                </div>
+				<div className="double-input-container">
+					<div className="small-input-combo-container">
+						<InputHeader value="SELECT CURRENCY" />
+						<div className="small-input-container">
+							<Input value={currency} 
+							onChange={(event) => {setCurrecncy(event.target.value)}} 
+							type="text" placeholder="XXXXXXXXXXXXXXXXX" />
+						</div>
+					</div>
+					<div className="small-input-combo-container">
+					<InputHeader value="SELECT DEPOSIT AMOUNT" />
+						<div className="small-input-container">
+						<Input value={depositAmount} 
+						onChange={(event) => {setDepositAmount(event.target.value)}} 
+						type="number" placeholder="XXXXXXXXXXXXXXXXX" />
+						</div>
+					</div>
+				</div>
+                <div className="create-buttons-container">
+                    <Button isDark={true} onClick={doDeposit}>Deposit</Button>
+                    <Button>Deploy New Vault</Button>
+                </div>
+            </div>
+            <Modal show={showReferralModal} handleClose={() => setShowReferralModal(false)}>
+                <span style={{"font-size": "18px"}}>
+                    {referralLink}
+                </span>
+            </Modal>
         </Page>
     )
 }
