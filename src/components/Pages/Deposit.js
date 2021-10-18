d1475b2d58cd0fa683a254ac9a74c624d78dc0d9
Rohun Carlton <rcarlton@usc.edu>
2021-10-17 21:27:00 -0700

    added all major deposit components

--- src/components/Pages/Deposit.js
+++ src/components/Pages/Deposit.js
@@ -1,40 +1,10 @@
-// import React from 'react'
-// import Page from "../Page/Page"
 import React, { useState } from 'react'
 import Page from "../Page/Page"
 import Button from "../Button/Button"
 import Input from "../Input/Input"
 import Modal from "../Modal/Modal"
 import InputHeader from "../Input/InputHeader"
-//import "./Create.css"
 
-// const Deposit = () => {
-//     const [charityAddress, setCharityAddress] = useState(null);
-//     const [giftRate, setGiftRate] = useState(null);
-//     const [charityName, setCharityName] = useState(null);
-
-//     const [showReferralModal, setShowReferralModal] = useState(false);
-//     const [referralLink, setReferralLink] = useState(null)
-
-//     const generateReferralLink = () => {
-//         var getUrl = window.location;
-//         var baseUrl = getUrl .protocol + "//" + getUrl.host + "/";
-        
-//         let referralLink;
-//         if (!charityAddress) {
-//             referralLink = "Must provide a charity wallet address.";
-//         } else if (!giftRate) {
-//             referralLink = "Must provide a gift rate.";
-//         } else if (!charityName) {
-//             referralLink = "Must provide a charity name.";
-//         } else {
-//             referralLink = `${baseUrl}deposit?address=${charityAddress}&rate=${giftRate}&name=${charityName}`;
-//         }
-        
-//         setReferralLink(referralLink);
-//         setShowReferralModal(true);
-// 	}
-	
 	const Deposit = () => {
 		const [charityAddress, setCharityAddress] = useState(null);
 		const [customInterestRate, setCustomInterestRate] = useState(null);
@@ -47,6 +17,8 @@ import InputHeader from "../Input/InputHeader"
 		const doDeposit = () => {
 			var getUrl = window.location;
 			var baseUrl = getUrl .protocol + "//" + getUrl.host + "/";
+			var select = document.getElementById("selectInterest");
+			console.log(select);
 			
 			let referralLink;
 			if (!charityAddress) {
@@ -68,21 +40,53 @@ import InputHeader from "../Input/InputHeader"
     return (
         <Page>
             <div className="create-container">
-                <div className="small-input-container">
-					<div>
-						<InputHeader value="SELECT CHARITY" />
-						<Input value={charityAddress} 
-						onChange={(event) => {setCharityAddress(event.target.value)}} 
-						type="text" placeholder="XXXXXXXXXXXXXXXXX" />
-					</div>
-					<div>
-						<InputHeader value="ENTER CUSTOM IINTEREST RATE" />
-						<Input value={customInterestRate}
-						onChange={(event) => {setCustomInterestRate(event.target.value)}}
-						type="text" placeholder="X.XX%"/>
-					</div>
-                </div>
+                
 				<div className="double-input-container">
+					<div class="small-input-combo-container">
+					<InputHeader value="SELECT CHARITY" />
+						<select id="selectInterest">
+							<option value="-1">N/A</option>
+							<option value="1%">Red Cross</option>
+							<option value="2%">Gates Foundation</option>
+							<option value="3%">PETA</option>
+						</select>
+					</div>
+					<div className="small-input-combo-container">
+						<InputHeader value="ENTER CUSTOM CHARITY ADDRESS" />
+						<div className="small-input-container">
+							<Input value={charityAddress} 
+							onChange={(event) => {setCharityAddress(event.target.value)}} 
+							type="text" placeholder="XXXXXXXXXXXXXXXXX" />
+						</div>
+					</div>
+				</div>
+
+				<br></br>
+
+				<div className="double-input-container">
+					<div class="small-input-combo-container">
+					<InputHeader value="SELECT INTEREST RATE" />
+						<select id="selectInterest">
+							<option value="-1">N/A</option>
+							<option value="1%">1%</option>
+							<option value="2%">2%</option>
+							<option value="3%">3%</option>
+							<option value="4%">4%</option>
+							<option value="5%">5%</option>
+						</select>
+					</div>
+					<div className="small-input-combo-container">
+						<InputHeader value="ENTER CUSTOM IINTEREST RATE" />
+						<div className="small-input-container">
+							<Input value={customInterestRate}
+							onChange={(event) => {setCustomInterestRate(event.target.value)}}
+							type="text" placeholder="X.XX%"/>
+						</div>
+					</div>
+				</div>
+				
+
+				<div className="centered-input-container">
 					<div className="small-input-combo-container">
 						<InputHeader value="SELECT CURRENCY" />
 						<div className="small-input-container">
@@ -100,6 +104,7 @@ import InputHeader from "../Input/InputHeader"
 						</div>
 					</div>
 				</div>
+		
                 <div className="create-buttons-container">
                     <Button isDark={true} onClick={doDeposit}>Deposit</Button>
                     <Button>Deploy New Vault</Button>
