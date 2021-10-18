9c8e17476cd56b74c3990b1d6baa8c8e39695963
Sajan Gutta <gutta@usc.edu>
2021-10-18 10:19:50 -0700

    Adding toggle to switch between showing custom and dropdown fields

--- src/components/Pages/Deposit.js
+++ src/components/Pages/Deposit.js
@@ -4,47 +4,58 @@ import Button from "../Button/Button"
 import Input from "../Input/Input"
 import Modal from "../Modal/Modal"
 import InputHeader from "../Input/InputHeader"
+import Toggle from "react-toggle";
+import "react-toggle/style.css"
 import "./Deposit.css"
 
-	const Deposit = () => {
-		const [charityAddress, setCharityAddress] = useState(null);
-		const [customInterestRate, setCustomInterestRate] = useState(null);
-		const [currency, setCurrecncy] = useState(null);
-		const [depositAmount, setDepositAmount] = useState(null);
+const Deposit = () => {
+	const [showCustomFields, setShowCustomFields] = useState(false);
 
-		const [showReferralModal, setShowReferralModal] = useState(false);
-    	const [referralLink, setReferralLink] = useState(null)
-	
-		const doDeposit = () => {
-			var getUrl = window.location;
-			var baseUrl = getUrl .protocol + "//" + getUrl.host + "/";
-			var select = document.getElementById("selectInterest");
-			console.log(select);
-			
-			let referralLink;
-			if (!charityAddress) {
-				referralLink = "Must provide a charity wallet address.";
-			} else if (!customInterestRate) {
-				referralLink = "Must provide a gift rate.";
-			} else if (!currency) {
-				referralLink = "Must provide a charity name.";
-			} else if (!depositAmount) {
-				referralLink = "Must provide deposit amount.";
-			} else {
-				referralLink = `${baseUrl}deposit?address=${charityAddress}&rate=${customInterestRate}&name=${currency}&depositAmount=${depositAmount}`;
-			}
-			
-			setReferralLink(referralLink);
-			setShowReferralModal(true);
+	const [charityAddress, setCharityAddress] = useState(null);
+	const [customInterestRate, setCustomInterestRate] = useState(null);
+	const [currency, setCurrecncy] = useState(null);
+	const [depositAmount, setDepositAmount] = useState(null);
+
+	const [showReferralModal, setShowReferralModal] = useState(false);
+	const [referralLink, setReferralLink] = useState(null)
+
+	const doDeposit = () => {
+		var getUrl = window.location;
+		var baseUrl = getUrl.protocol + "//" + getUrl.host + "/";
+		var select = document.getElementById("selectInterest");
+		console.log(select);
+
+		let referralLink;
+		if (!charityAddress) {
+			referralLink = "Must provide a charity wallet address.";
+		} else if (!customInterestRate) {
+			referralLink = "Must provide a gift rate.";
+		} else if (!currency) {
+			referralLink = "Must provide a charity name.";
+		} else if (!depositAmount) {
+			referralLink = "Must provide deposit amount.";
+		} else {
+			referralLink = `${baseUrl}deposit?address=${charityAddress}&rate=${customInterestRate}&name=${currency}&depositAmount=${depositAmount}`;
 		}
 
-    return (
-        <Page>
-            <div className="create-container">
-                
+		setReferralLink(referralLink);
+		setShowReferralModal(true);
+	}
+
+	return (
+		<Page>
+			<div className="create-container">
+				<div className="toggle-container">
+					<Toggle
+						defaultChecked={showCustomFields}
+						icons={false}
+						onChange={() => setShowCustomFields(!showCustomFields)} />
+					<span className="toggle-label">Enter Custom Charity Info?</span>
+				</div>
+
 				<div className="double-input-container">
 					<div className="small-input-combo-container">
-					<InputHeader value="SELECT CHARITY" />
+						<InputHeader value="SELECT CHARITY" />
 						<select id="selectInterest" className="dropdown-container">
 							<option value="-1">N/A</option>
 							<option value="1%">Red Cross</option>
@@ -55,9 +66,9 @@ import "./Deposit.css"
 					<div className="small-input-combo-container">
 						<InputHeader value="ENTER CUSTOM CHARITY ADDRESS" />
 						<div className="small-input-container">
-							<Input value={charityAddress} 
-							onChange={(event) => {setCharityAddress(event.target.value)}} 
-							type="text" placeholder="XXXXXXXXXXXXXXXXX" />
+							<Input value={charityAddress}
+								onChange={(event) => { setCharityAddress(event.target.value) }}
+								type="text" placeholder="XXXXXXXXXXXXXXXXX" />
 						</div>
 					</div>
 				</div>
@@ -68,7 +79,7 @@ import "./Deposit.css"
 
 				<div className="double-input-container">
 					<div class="small-input-combo-container">
-					<InputHeader value="SELECT INTEREST RATE" />
+						<InputHeader value="SELECT INTEREST RATE" />
 						<div>
 							<select id="selectInterest" className="dropdown-container">
 								<option value="-1">N/A</option>
@@ -84,12 +95,12 @@ import "./Deposit.css"
 						<InputHeader value="ENTER CUSTOM IINTEREST RATE" />
 						<div className="small-input-container">
 							<Input value={customInterestRate}
-							onChange={(event) => {setCustomInterestRate(event.target.value)}}
-							type="text" placeholder="X.XX%"/>
+								onChange={(event) => { setCustomInterestRate(event.target.value) }}
+								type="text" placeholder="X.XX%" />
 						</div>
 					</div>
 				</div>
-				
+
 				<div className="centered-text">
 					AND
 				</div>
@@ -98,35 +109,35 @@ import "./Deposit.css"
 					<div className="small-input-combo-container">
 						<InputHeader value="SELECT CURRENCY" />
 						<div className="small-input-container">
-							<Input value={currency} 
-							onChange={(event) => {setCurrecncy(event.target.value)}} 
-							type="text" placeholder="XXXXXXXXXXXXXXXXX" />
+							<Input value={currency}
+								onChange={(event) => { setCurrecncy(event.target.value) }}
+								type="text" placeholder="XXXXXXXXXXXXXXXXX" />
 						</div>
 					</div>
 				</div>
 				<div className="centered-input-container">
 					<div className="small-input-combo-container">
-					<InputHeader value="SELECT DEPOSIT AMOUNT" />
+						<InputHeader value="SELECT DEPOSIT AMOUNT" />
 						<div className="small-input-container">
-						<Input value={depositAmount} 
-						onChange={(event) => {setDepositAmount(event.target.value)}} 
-						type="number" placeholder="XXXXXXXXXXXXXXXXX" />
+							<Input value={depositAmount}
+								onChange={(event) => { setDepositAmount(event.target.value) }}
+								type="number" placeholder="XXXXXXXXXXXXXXXXX" />
 						</div>
 					</div>
 				</div>
-		
-                <div className="create-buttons-container">
-                    <Button isDark={true} onClick={doDeposit}>Deposit</Button>
-                    <Button>Deploy New Vault</Button>
-                </div>
-            </div>
-            <Modal show={showReferralModal} handleClose={() => setShowReferralModal(false)}>
-                <span style={{"font-size": "18px"}}>
-                    {referralLink}
-                </span>
-            </Modal>
-        </Page>
-    )
+
+				<div className="create-buttons-container">
+					<Button isDark={true} onClick={doDeposit}>Deposit</Button>
+					<Button>Deploy New Vault</Button>
+				</div>
+			</div>
+			<Modal show={showReferralModal} handleClose={() => setShowReferralModal(false)}>
+				<span style={{ "font-size": "18px" }}>
+					{referralLink}
+				</span>
+			</Modal>
+		</Page>
+	)
 }
 
 export default Deposit;
