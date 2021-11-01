260c092193f0cece131b4e7fc873e585cc7cf61d
nicolasneven <nneven@usc.edu>
2021-11-01 12:14:20 -0700

    update ABIs + contract addresses

--- src/config.js
+++ src/config.js
@@ -4,7 +4,8 @@ import charityVaultABI from './contracts/CharityVault.sol/CharityVault.json';
 export const CharityVaultABI = charityVaultABI;
 export const CharityVaultFactoryABI = charityVaultFactoryABI;
 
-export const CharityVaultFactoryDeployedAddress = '0x293e3a98CC905e759EDB07d579fa2Cdb24941575';
+export const CharityVaultDeployedAddress = "0xf25e32C0f2928F198912A4F21008aF146Af8A05a";
+export const CharityVaultFactoryDeployedAddress = "0x2a0b356D73C4A4090C1b5ef8209f8961dD53a02D";
 
 export const WalletConnectInfuraId = "782109c6748c48d6b91c3eafa72b5292";
 export const Network = "goerli";
@@ -17,6 +18,6 @@ export const Tokens = {
 
 export const Charities = {
 	"Charity 1": "0x77e34153d9CbAD9f457b6DDFa61B721Ec2e92F52",
-	"Charity 2": "address-here",
-	"Charity 3": "address-here"
+	"Charity 2": "0xBB379331De54A7c0a4b2bfF5A54A14cdba7E9E6d",
+	"Charity 3": "0xf25e32C0f2928F198912A4F21008aF146Af8A05a"
 }
